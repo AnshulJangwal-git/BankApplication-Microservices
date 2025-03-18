82266640bcdeb1352475988429105d2b3c5aca04
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-19 00:39:04 +0530

    up

---
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/service/AccountsService.java
@@ -0,0 +1,10 @@
+package com.eazybytes.accounts.service;
+
+
+import com.eazybytes.accounts.dto.CustomerDTO;
+
+public interface AccountsService {
+
+    void createAccount(CustomerDTO customerDTO);
+
+}
---
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.dto.CustomerDTO;
+import com.eazybytes.accounts.service.AccountsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountsServiceImpl implements AccountsService {
+
+    @Override
+    public void createAccount(CustomerDTO customerDTO) {
+        
+    }
+}
