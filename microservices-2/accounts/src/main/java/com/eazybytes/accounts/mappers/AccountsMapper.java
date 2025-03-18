25eb2f84663823040812cfd557cb7869224533c4
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-19 00:48:48 +0530

    up

---
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/mappers/AccountsMapper.java
@@ -0,0 +1,4 @@
+package com.eazybytes.accounts.mappers;
+
+public class AccountsMapper {
+}
--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -1,14 +1,22 @@
 package com.eazybytes.accounts.service.impl;
 
 import com.eazybytes.accounts.dto.CustomerDTO;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.AccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AccountsServiceImpl implements AccountsService {
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
 
     @Override
     public void createAccount(CustomerDTO customerDTO) {
-        
+
     }
 }
