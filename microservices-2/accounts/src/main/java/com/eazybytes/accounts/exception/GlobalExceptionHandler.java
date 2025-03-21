dace78998d261b438909986817e85ca357e8e1f3
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-21 23:51:46 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -13,6 +13,18 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(Exception exception,
+                                                                            WebRequest webRequest) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException exception,
                                                                             WebRequest webRequest) {
@@ -38,5 +50,7 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
     }
+
+
 
 }
