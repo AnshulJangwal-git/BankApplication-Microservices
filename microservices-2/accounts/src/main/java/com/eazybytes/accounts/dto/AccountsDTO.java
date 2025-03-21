fc49a666972967597735de4662f58eed755a3174
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-22 00:03:48 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
@@ -1,11 +1,20 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDTO {
+
+    @NotEmpty(message = "Account Number can not be empty")
+    @Pattern(regexp = "[0-9]{10}", message = "Account Number must be exactly 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type can not be empty")
     private String accountType;
+
+    @NotEmpty(message = "Branch Address can not be null or empty")
     private String branchAddress;
 
 }
