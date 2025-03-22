fb2abca0d3c3ede133e37945f3a2ec23bdfd2fb4
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-22 17:08:58 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
@@ -16,4 +16,11 @@ public class ErrorResponseDTO {
     private String errorMessage;
     private LocalDateTime errorTime;
 
+//    public ErrorResponseDTO(String apiPath, HttpStatus errorCode, String errorMessage,
+//                            LocalDateTime errorTime) {
+//        this.apiPath = apiPath;
+//        this.errorCode = errorCode;
+//        this.errorMessage = errorMessage;
+//        this.errorTime = errorTime;
+//    }
 }
