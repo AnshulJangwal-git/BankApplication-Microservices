2818e38cfa82aa11cad95a1680cf9c94e2fb2168
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-22 22:06:24 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account Microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Anshul Jangwal",
+						email = "anshuljangwal@gmail.com",
+						url = "https://github.com/AnshulJangwal-git"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/AnshulJangwal-git"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
