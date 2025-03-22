77ff8899622e4b75c6effd9427e195f9ed76578a
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-23 00:15:50 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
@@ -15,12 +15,4 @@ public class ErrorResponseDTO {
     private HttpStatus errorCode;
     private String errorMessage;
     private LocalDateTime errorTime;
-
-//    public ErrorResponseDTO(String apiPath, HttpStatus errorCode, String errorMessage,
-//                            LocalDateTime errorTime) {
-//        this.apiPath = apiPath;
-//        this.errorCode = errorCode;
-//        this.errorMessage = errorMessage;
-//        this.errorTime = errorTime;
-//    }
 }
--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDTO.java
@@ -1,11 +1,24 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        name = "Response",
+        description = "Schema to hold Successful Response Information"
+)
 public class ResponseDTO {
+
+    @Schema(
+            description = "Status code of the EazyBank Account", example = "200"
+    )
     private String statusCode;
+
+    @Schema(
+            description = "Status message of the EazyBank Account", example = "Request Processed Successfully"
+    )
     private String statusMsg;
 }
