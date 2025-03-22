b06f12aea72398fa4c915e897561156fa764dab4
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-23 00:12:48 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
@@ -1,19 +1,33 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account Information"
+)
 public class AccountsDTO {
 
+    @Schema(
+            description = "Account number of the EazyBank Account"
+    )
     @NotEmpty(message = "Account Number can not be empty")
     @Pattern(regexp = "[0-9]{10}", message = "Account Number must be exactly 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of the EazyBank Account"
+    )
     @NotEmpty(message = "Account type can not be empty")
     private String accountType;
 
+    @Schema(
+        description = "Branch Address of the EazyBank Account"
+    )
     @NotEmpty(message = "Branch Address can not be null or empty")
     private String branchAddress;
 
