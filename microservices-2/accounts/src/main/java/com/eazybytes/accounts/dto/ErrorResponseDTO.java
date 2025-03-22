b247c90ca2f7b1ff5543d777fbe4af71bee7ab4a
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-23 00:19:48 +0530

    up

--- microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
+++ microservices-2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDTO.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,29 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Schema(
+        name = "Error Response",
+        description = "Schema to hold Successful Error Response Information"
+)
 public class ErrorResponseDTO {
+
+    @Schema(
+            description = "API Path invoked by the Client"
+    )
     private String apiPath;
+    @Schema(
+            description = "Error Code representing the error happened"
+    )
     private HttpStatus errorCode;
+
+    @Schema(
+            description = "Error Message representing the error happened"
+    )
     private String errorMessage;
+
+    @Schema(
+        description = "Time when the error happened"
+    )
     private LocalDateTime errorTime;
 }
